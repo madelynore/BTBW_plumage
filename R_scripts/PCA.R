41adefa0b217f945c85308add03fe6c6e7eb0b68
Madelyn Ore <madelynore@gmail.com>
2026-06-03 17:14:13 -0400

    save before cleaning

--- R_scripts/PCA.R
+++ R_scripts/PCA.R
@@ -140,7 +140,7 @@ asy <- avg_img %>%
 
 #filtering for just dorsum measures, removing lum bc redundant with dbl and area bc it's not relevant for this patch
 dorsum <- avg_img %>% 
-  dplyr::select(ends_with("_d"), -lumSD_d,-lumMean_d, -area_mm2_d) %>% 
+  dplyr::select(ends_with("_d"), -lumSD_d,-lumMean_d, -area_mm2_d, -PC1_d) %>% 
   na.omit()
 
 
@@ -148,7 +148,7 @@ d_pca <- prcomp(~ ., data = dorsum)
 
 screeplot(d_pca, type="lines")
 
-ggbiplot(d_pca, choices = c(1,2))+
+dplot <- ggbiplot(d_pca, choices = c(1,2))+
   theme_classic()
 
 dorsum_pcs <- cbind(dorsum, predict(d_pca))
@@ -157,7 +157,7 @@ plot(dorsum_pcs$dblMean_d, dorsum_pcs$PC1)
 
 #filtering for just throat measures, removing lum bc redundant with dbl and area bc it's not relevant for this patch
 throat <- asy %>% 
-  dplyr::select(ends_with("_t"), -lumSD_t,-lumMean_t, -area_mm2_t) %>% 
+  dplyr::select(ends_with("_t"), -lumSD_t,-lumMean_t, -area_mm2_t, PC1_t) %>% 
   na.omit()
 
 
@@ -165,7 +165,7 @@ t_pca <- prcomp(~ ., data = throat)
 
 screeplot(t_pca, type="lines")
 
-ggbiplot(t_pca, choices = c(1,2))+
+tplot <- ggbiplot(t_pca, choices = c(1,2))+
   ylim(c(-2,2)) +
   theme_classic()
 
@@ -175,7 +175,7 @@ plot(throat_pcs$dblMean_t, throat_pcs$PC1)
 
 #filtering for just covert measures, removing lum bc redundant with dbl and area bc it's not relevant for this patch
 covert <- asy %>% 
-  dplyr::select(ends_with("_o"), -lumSD_o,-lumMean_o, -area_mm2_o) %>% 
+  dplyr::select(ends_with("_o"), -lumSD_o,-lumMean_o, -area_mm2_o, -PC1_o) %>% 
   na.omit()
 
 
@@ -183,7 +183,7 @@ o_pca <- prcomp(~ ., data = covert)
 
 screeplot(o_pca, type="lines")
 
-ggbiplot(o_pca, choices = c(1,2))+
+oplot <- ggbiplot(o_pca, choices = c(1,2))+
   theme_classic()
 
 covert_pcs <- cbind(covert, predict(o_pca))
@@ -192,7 +192,7 @@ plot(covert_pcs$dblMean_o, covert_pcs$PC1)
 
 #filtering for just belly measures, removing lum bc redundant with dbl and area bc it's not relevant for this patch
 belly <- asy %>% 
-  dplyr::select(ends_with("_b"), -lumSD_b,-lumMean_b, -area_mm2_b) %>% 
+  dplyr::select(ends_with("_b"), -lumSD_b,-lumMean_b, -area_mm2_b, -PC1_b) %>% 
   na.omit()
 
 
@@ -200,7 +200,7 @@ b_pca <- prcomp(~ ., data = belly)
 
 screeplot(b_pca, type="lines")
 
-ggbiplot(b_pca, choices = c(1,2))+
+bplot <- ggbiplot(b_pca, choices = c(1,2))+
   theme_classic()
 
 belly_pcs <- cbind(belly, predict(b_pca))
@@ -209,7 +209,7 @@ plot(belly_pcs$dblMean_b, belly_pcs$PC1)
 
 #filtering for just crown measures, removing lum bc redundant with dbl and area bc it's not relevant for this patch
 crown <- asy %>% 
-  dplyr::select(ends_with("_c"), -lumSD_c,-lumMean_c, -area_mm2_c) %>% 
+  dplyr::select(ends_with("_c"), -lumSD_c,-lumMean_c, -area_mm2_c, -PC1_c) %>% 
   na.omit()
 
 
@@ -217,7 +217,7 @@ c_pca <- prcomp(~ ., data = crown)
 
 screeplot(c_pca, type="lines")
 
-ggbiplot(c_pca, choices = c(1,2))+
+cplot <- ggbiplot(c_pca, choices = c(1,2))+
   theme_classic()
 
 crown_pcs <- cbind(crown, predict(c_pca))
@@ -227,7 +227,7 @@ plot(crown_pcs$dblMean_c, crown_pcs$PC1)
 
 #filtering for just wingspot measures, removing lum bc redundant with dbl and area bc i just want color
 wingspot <- asy %>% 
-  dplyr::select(ends_with("_w"), -lumSD_w,-lumMean_w, -area_mm2_w) %>% 
+  dplyr::select(ends_with("_w"), -lumSD_w,-lumMean_w, -area_mm2_w, -PC1_w) %>% 
   na.omit()
 
 
@@ -235,9 +235,42 @@ w_pca <- prcomp(~ ., data = wingspot)
 
 screeplot(w_pca, type="lines")
 
-ggbiplot(w_pca, choices = c(1,2))+
+wplot <- ggbiplot(w_pca, choices = c(1,2))+
   theme_classic()
 
 wingspot_pcs <- cbind(wingspot, predict(w_pca))
 
 plot(wingspot_pcs$dblMean_w, wingspot_pcs$PC1)
+
+
+
+# combine -----------------------------------------------------------------
+
+library(patchwork)
+# Fix each biplot with consistent coord limits and remove legends
+fix_biplot <- function(p, title = NULL) {
+  p + 
+    coord_equal(xlim = c(-3, 3), ylim = c(-3, 3)) +  # uniform axis limits
+    theme_classic() +
+    theme(
+      legend.position = "none",           # remove legends (add one later if needed)
+      plot.title = element_text(hjust = 0.5)
+    ) +
+    if (!is.null(title)) ggtitle(title) else NULL
+}
+
+# Apply to each plot
+bplot <- fix_biplot(ggbiplot(b_pca, choices = c(1,2)))
+oplot <- fix_biplot(ggbiplot(o_pca, choices = c(1,2)))
+cplot <- fix_biplot(ggbiplot(c_pca, choices = c(1,2)))
+dplot <- fix_biplot(ggbiplot(d_pca, choices = c(1,2)))
+tplot <- fix_biplot(ggbiplot(t_pca, choices = c(1,2)))
+wplot <- fix_biplot(ggbiplot(w_pca, choices = c(1,2)))
+
+# Combine
+pcaplots <- bplot + oplot + cplot + dplot + tplot + wplot +
+  plot_layout(ncol = 2) +
+  plot_annotation(tag_levels = 'A')
+
+ggsave(filename = "results/Color_PCA_plot.png", plot = pcaplots, 
+       width = 12, height = 18, dpi = 600)
