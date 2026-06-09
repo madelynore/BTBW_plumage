aabd4d5b3b79108da625eecc0598a0368aace469
Madelyn Ore <madelynore@gmail.com>
2026-06-09 11:54:06 -0400

    updated/cleaned env

--- R_scripts/Environment_by_individual.R
+++ R_scripts/Environment_by_individual.R
@@ -5,36 +5,14 @@
 library(dplyr)
 library(raster)
 library(sp)
-library(rgdal)
 
 # extract data points from each individual's location for each environ --------
 ##extract for specific lat/long
-btbw_samp <- read.csv(file = "~/Documents/Cornell/BTBW_geographic_coloration/BTBW_plumage/data/BTBW_whole_specimen_Image_Analysis_measurements_averaged_allpop.csv") %>% 
+btbw_samp <- read.csv(file = "data/BTBW_whole_specimen_Image_Analysis_measurements_averaged_allpop.csv") %>% 
   dplyr::select(ID, lon, lat) %>% 
   group_by(ID) %>% 
   summarise(lon = mean(lon), lat = mean(lat))
 
-# Bioclim -----------------------------------------------------------------
-
-setwd("~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/wc2.1_30s_bio/")
-bioclim_files <- list.files(pattern = "wc.*")
-
-## create a loop to make bioclim df
-for (i in 1:length(bioclim_files)){
-  climname <- str_extract(bioclim_files, "bio_\\d+")[i]
-  bio <- raster(bioclim_files[i])
-  bioclim <-raster::extract(bio,btbw_samp[,2:3],fun=mean)
-  btbw_samp[[climname]] <- bioclim
-  }
-
-temp <- btbw_samp %>% 
-  dplyr::select(bio_1, bio_2, bio_3, bio_4, bio_5, bio_6, bio_7, bio_8, bio_9, bio_10, bio_11,
-                bio_12, bio_13, bio_14, bio_15, bio_16, bio_17, bio_18, bio_19)
-
-cormat <- cor(temp)
-
-length(which(cormat > abs(0.7)))
-dim(cormat)
 
 # ClimateNA -----------------------------------------------------------------
 
@@ -76,70 +54,18 @@ climna_sum <- btbw_samp %>%
 
 
 # SRTM --------------------------------------------------------------------
-setwd("~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/srtm/")
-list.files()
-srtm=raster("srtm.tif")
+list.files(path = "~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/srtm/")
+srtm=raster("~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/srtm/srtm.tif")
 coord <- btbw_samp[,2:3]
 srtm_btbw<-raster::extract(srtm,coord,fun=mean)
 
 
 # NDVI --------------------------------------------------------------------
-setwd("~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/LandCover/")
-list.files()
-ndvi=raster("ndvimax.tif")
+list.files("~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/LandCover/")
+ndvi=raster("~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/LandCover/ndvimax.tif")
 coord <- btbw_samp[,2:3]
 ndvi_btbw<-raster::extract(ndvi,coord,fun=mean)
 
-
-# surface moisture --------------------------------------------------------
-
-setwd("~/Documents/Cornell/Genoscape BTBW/BTBW-GEA/data_raw/environ_data/Qscat_SurfaceMoisture/")
-list.files()
-qscat=raster("qscat.tif")
-coord <- btbw_samp[,2:3]
-qscat_btbw<-raster::extract(qscat,coord,fun=mean)
-
-      
-# extracting Tave08 for year prior to collection (temp during molt) -------
-setwd("~/Documents/Cornell/BTBW_geographic_coloration/BTBW_plumage/")
-
-
-idy <- read.csv("data/BTBW_whole_specimen_Image_Analysis_measurements_allpop_avgimg_wide.csv") %>% 
-  dplyr::select(ID, Year)
-
-idy$Year <- as.numeric(paste0(19,idy$Year))
-
-idy$Tave08_molt <- NA
-
-## 606619 is missing from the climateNA files for some reason, but it's the same coords as 606621, so just going to remove it and duplicate 606621 at end
-id606619 <- idy[which(idy$ID == 606619),]
-idy <- idy[-which(idy$ID == 606619),]
-# 613610 is duplicated and one row doesn't have a year
-idy <- idy[-which(idy$ID == 613610 & is.na(idy$Year)),]
-
-
-# Loop through each row of the data frame
-for (i in 1:nrow(idy)) {
-  #pull out ID
-  sample_id <- idy$ID[i]
-  year <- idy$Year[i]
-  
-  # Calculate the Year - 1
-  year_minus_1 <- year - 1
-  
-  # Construct the file name for the corresponding CSV
-  file_name <- paste0("data_raw/climateNArdl/BTBW_specimen_coords_Year_", year_minus_1, ".csv")
-  csv_data <- read.csv(file_name)
-  
-  # Find the row in the CSV that matches the SampleID
-  Tave08 <- csv_data$Tave08[which(csv_data$ID1 == sample_id)]
-  
-  idy$Tave08_molt[i] <- Tave08
-}
-
-id606619$Tave08_molt <- idy$Tave08_molt[which(idy$ID == 606621)]
-
-idy_all <- rbind(id606619, idy)
 
 # combine all together ----------------------------------------------------
 #reset wd
@@ -147,9 +73,7 @@ setwd("~/Documents/Cornell/BTBW_geographic_coloration/BTBW_plumage/")
 
 biona <- merge(btbw_samp, climna_sum, all= T)
 
-bionamolt <- merge(biona, idy_all, all = T)
-
-btbw_env <- cbind(bionamolt, qscat = qscat_btbw, ndvi_M = ndvi_btbw, srtm = srtm_btbw)
+btbw_env <- cbind(biona, ndvi_M = ndvi_btbw, srtm = srtm_btbw)
 
 write.table(btbw_env, "data/Env_by_individual_specimen_samples.txt",
             row.names = F, quote = F, col.names = T)
